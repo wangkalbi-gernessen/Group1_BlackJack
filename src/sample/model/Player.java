8bc0abf404b736d2e1f48a9d7d6e7b1f45b415a6
YumiMachino <y.y.1515m@gmail.com>
2020-11-19 16:31:17 -0800

    Update Card, controller, fxml file

--- src/sample/model/Player.java
+++ src/sample/model/Player.java
@@ -1,17 +1,21 @@
 package sample.model;
 
+import java.util.ArrayList;
+
 /**
  * This class is to represent players (Dealer, player1, player2)
  */
 public class Player {
     private String name;    // Dealer, player1, player2
-    private Card[] hand;   //  to track which card(value/suit) the player holds
+    private ArrayList<Integer> hand; //  to track which card(value/suit) the player holds
     private int sum;        // sum of cards picked
+    private boolean playing;
 
-    public Player(String name, Card[] hand, int sum){
+    public Player(String name, ArrayList<Integer> hand, int sum, boolean playing){
         this.name = name;
         this.hand = hand;
         this.sum = sum;
+        this.playing = playing;
     }
 
 
@@ -19,24 +23,31 @@ public class Player {
     public String getName(){
         return name;
     }
-    public Card[] getHand(){
+    public ArrayList<Integer> getHand(){
         return hand;
     }
 
     public int getSum(){
         return sum;
     }
+    public boolean getPlaying(){
+        return playing;
+    }
 
     // setter methods
     public void setName(String name){
         this.name = name;
     }
-    public void setHand(Card[] hand) {
+    public void setHand(ArrayList<Integer> hand) {
         this.hand = hand;
     }
 
     public void setSum(int sum) {
         this.sum = sum;
+    }
+
+    public void setPlaying(boolean playing){
+        this.playing = playing;
     }
 
 }
