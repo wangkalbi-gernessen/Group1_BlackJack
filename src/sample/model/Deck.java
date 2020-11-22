6dbb80abdc7e59b051d51a47615d01785d61fa05
YumiMachino <y.y.1515m@gmail.com>
2020-11-21 22:04:02 -0800

    Add enable/disable button method and track each turn, Update deck final constants

--- src/sample/model/Deck.java
+++ src/sample/model/Deck.java
@@ -1,30 +1,29 @@
 package sample.model;
 
 import java.util.ArrayList;
-//import java.util.HashMap;
 
 public class Deck {
 
     // Suit Constants
-    private static String HEARTS = "♥";
-    private static String DIAMONDS = "♦";
-    private static String SPADES = "♠";
-    private static String CLOVERS = "♣";
+    private static final String HEARTS = "♥";
+    private static final String DIAMONDS = "♦";
+    private static final String SPADES = "♠";
+    private static final String CLOVERS = "♣";
 
     // Rank Constants
-    private static int ACE = 11;
-    private static int TWO = 2;
-    private static int THREE = 3;
-    private static int FOUR = 4;
-    private static int FIVE = 5;
-    private static int SIX = 6;
-    private static int SEVEN = 7;
-    private static int EIGHT = 8;
-    private static int NINE = 9;
-    private static int TEN = 10;
-    private static int JACK = 10;
-    private static int QUEEN = 10;
-    private static int KING = 10;
+    private static final int ACE = 11;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    private static final int FOUR = 4;
+    private static final int FIVE = 5;
+    private static final int SIX = 6;
+    private static final int SEVEN = 7;
+    private static final int EIGHT = 8;
+    private static final int NINE = 9;
+    private static final int TEN = 10;
+    private static final int JACK = 10;
+    private static final int QUEEN = 10;
+    private static final int KING = 10;
 
     private static ArrayList<Card> deckOfCards;
 
@@ -43,23 +42,9 @@ public class Deck {
 
     public static ArrayList<Card> refillDeck(){
         ArrayList<Card> deckOfCards = new ArrayList<>();
-//        deckOfCards.add(new Card(HEARTS, ACE));
-//        deckOfCards.add(new Card(HEARTS, TWO));
-//        deckOfCards.add(new Card(HEARTS, THREE));
-//        deckOfCards.add(new Card(HEARTS, FOUR));
-//        deckOfCards.add(new Card(HEARTS, FIVE));
-//        deckOfCards.add(new Card(HEARTS, SIX));
-//        deckOfCards.add(new Card(HEARTS, SEVEN));
-//        deckOfCards.add(new Card(HEARTS, EIGHT));
-//        deckOfCards.add(new Card(HEARTS, NINE));
-//        deckOfCards.add(new Card(HEARTS, TEN));
-//        deckOfCards.add(new Card(HEARTS, JACK));
-//        deckOfCards.add(new Card(HEARTS, QUEEN));
-//        deckOfCards.add(new Card(HEARTS, KING));
 
         String[] suit = {HEARTS, DIAMONDS, SPADES, CLOVERS};
         int[] rank = {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
-//        deckOfCards.add(new Card(suit[0],rank[0]));
 
         for (int i = 0; i < 4; i++){
          for (int j = 0; j < 13; j++){
